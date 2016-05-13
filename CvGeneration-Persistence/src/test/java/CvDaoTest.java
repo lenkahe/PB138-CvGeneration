9bcb90f3354bb185e09abee0e5c94bc4753976fb
pajajezek <pajajezek@gmail.com>
2016-05-14 00:20:31 +0200

    Added TestDao jUnit

--- CvGeneration-Persistence/src/test/java/CvDaoTest.java
+++ CvGeneration-Persistence/src/test/java/CvDaoTest.java
@@ -6,11 +6,17 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.XMLResource;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Test for data access object for CV.
  *
- * @author Lenka Heldova
+ * @author Pavel Jezek
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = DaoConfiguration.class)
@@ -20,9 +26,26 @@ public class CvDaoTest {
     private CvDao cvDao;
 
     @Test
-    public void test(){
-        Assert.assertEquals(true,true);
+    public void TestCvDaoComplete() throws XMLDBException, FileNotFoundException {
+        File file;
+        String fileName, fileContent;
+        XMLResource xmlResource;
+
+        //save
+        file = new File("src/test/java/cv.xml");
+        fileName = cvDao.saveResource(file).getDocumentId();
+
+        //get
+        xmlResource = cvDao.getResource(fileName);
+        fileContent = new Scanner(file).useDelimiter("\\Z").next();
+        fileContent = fileContent.replace("\n", "").replace("\r", "").replace("    ", "");
+
+        Assert.assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" + xmlResource.getContent(), fileContent);
+
+        //delete
+        cvDao.deleteResource(fileName);
+        xmlResource = cvDao.getResource(fileName);
+
+        Assert.assertEquals(xmlResource, null);
     }
-
-
 }
