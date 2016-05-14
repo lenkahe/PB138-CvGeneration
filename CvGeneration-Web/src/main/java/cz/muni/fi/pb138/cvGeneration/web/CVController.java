4c684361696d275ee5efc921f6089e9bfba5a020
Marek <mperichta@gmail.com>
2016-05-14 02:43:25 +0200

    Web preparations
    + pre-created views and controller
    + deployment descriptor (web.xml)
    + added spring mvc and jsp dependencies

---
+++ CvGeneration-Web/src/main/java/cz/muni/fi/pb138/cvGeneration/web/CVController.java
@@ -0,0 +1,15 @@
+package cz.muni.fi.pb138.cvGeneration.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Marek Perichta on 14.05.2016.
+ *
+ * This controller will manipulate with CV model and create show the views using different methods.
+ */
+
+
+@Controller
+public class CVController {
+
+}
