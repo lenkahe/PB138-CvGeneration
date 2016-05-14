a3d994fafbcaa4f0e04c4e3ecc2ade7da30ac4ac
Marek <mperichta@gmail.com>
2016-05-14 13:03:17 +0200

    Added URL sharing class and configured application context

--- CvGeneration-Web/src/main/java/cz/muni/fi/pb138/cvGeneration/web/CVController.java
+++ CvGeneration-Web/src/main/java/cz/muni/fi/pb138/cvGeneration/web/CVController.java
@@ -1,15 +1,17 @@
 package cz.muni.fi.pb138.cvGeneration.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Marek Perichta on 14.05.2016.
  *
- * This controller will manipulate with CV model and create show the views using different methods.
+ * This controller will manipulate with CV model and create the views using different methods.
  */
 
 
 @Controller
+@RequestMapping("/")
 public class CVController {
 
 }
---
+++ CvGeneration-Web/src/main/java/cz/muni/fi/pb138/cvGeneration/web/utils/ClientWebUrl.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pb138.cvGeneration.web.utils;
+
+/**
+ * Created by Marek Perichta on 14.05.2016.
+ *
+ * Definition of URL constants in CvGeneration web client.
+ */
+public class ClientWebUrl {
+
+    public static final String LOGIN =
+            "/login";
+}
