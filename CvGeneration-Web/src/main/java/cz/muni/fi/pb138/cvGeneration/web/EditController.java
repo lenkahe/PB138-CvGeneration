43a4d1a48c5763bd5a75687568bf1435627f3952
Lenka Heldová <lenka.heldova@unicornsystems.eu>
2016-06-03 10:56:35 +0200

    fix path to schema

--- CvGeneration-Web/src/main/java/cz/muni/fi/pb138/cvGeneration/web/EditController.java
+++ CvGeneration-Web/src/main/java/cz/muni/fi/pb138/cvGeneration/web/EditController.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pb138.cvGeneration.web;
 
+
 import cz.muni.fi.pb138.cvGeneration.api.service.CvService;
 import cz.muni.fi.pb138.cvGeneration.entity.Person;
 import cz.muni.fi.pb138.cvGeneration.entity.item.PersonalInfo;
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,7 +22,7 @@ import java.util.Map;
 public class EditController {
 
     @Autowired
-    CvService cvService;
+    private CvService cvService;
 
     @RequestMapping(value="/save",method= RequestMethod.POST)
     public void saveCv(HttpServletRequest request, HttpServletResponse response) {
